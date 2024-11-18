f2858c66b540ddf4bb862c3eb50bbc7a0a94fdc4
mirinae700 <mirinae219@gmail.com>
2024-11-18 20:28:33 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 14.09 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -5,12 +5,11 @@ class Solution {
     private int opEnd;
     
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        int currentPos = convertToSeconds(pos);
         this.videoLength = convertToSeconds(video_len);
         this.opStart = convertToSeconds(op_start);
         this.opEnd = convertToSeconds(op_end);
         
-        if (!validate(commands)) return timeFormat(currentPos);
+        int currentPos = convertToSeconds(pos);
         if (isOpeningTime(currentPos)) {
             currentPos = opEnd;
         }
@@ -49,14 +48,5 @@ class Solution {
     private int convertToSeconds(String time) {
         String[] arr = time.split(":");
         return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
-    }
-    
-    private boolean validate(String[] commands) {
-        for (String command : commands) {
-            if (!(command.equals("prev") || command.equals("next"))) {
-                return false;
-            }
-        }
-        return commands.length >= 1 && commands.length <= 100;
     }
 }
