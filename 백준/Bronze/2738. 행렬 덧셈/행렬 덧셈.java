daff3a78ec7c8be1efdda0b164fba7ee23cf963c
Yujin Jeon <mirinae219@gmail.com>
2024-11-24 02:41:34 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 148 ms, Memory: 15712 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -14,7 +13,7 @@ public class Main {
         for (int i = 0; i < array.length; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < array[0].length; j++) {
-                array[i][j] += Integer.parseInt(st.nextToken());
+                array[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
