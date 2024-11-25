23fed74683e9c6d254903982a6118eca7bf2a9be
Yujin Jeon <mirinae219@gmail.com>
2024-11-25 23:35:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] charArray = br.readLine().toCharArray();
+        int index = Integer.parseInt(br.readLine());
+
+        System.out.println(charArray[index - 1]);
+    }
+}
