4ccea69040f10d46d6d59a683a53427ba0fccbc8
Yujin Jeon <mirinae219@gmail.com>
2024-11-26 22:12:07 +0900

    [Bronze IV] Title: 세수정렬, Time: 100 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let arr = input[0].split(" ").map(Number);
+arr.sort((a, b) => {return a - b});
+
+let result = arr.reduce((a, b) => a.toString() + " " + b.toString());
+console.log(result);
