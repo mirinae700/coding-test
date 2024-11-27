cfa0d81b6cb348f811905d00093f2a419ab3e3ef
Yujin Jeon <mirinae219@gmail.com>
2024-11-28 00:18:26 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 9620 KB -BaekjoonHub
    - 정렬 코드 불필요하여 삭제
    - 동전 별 개수 세는 로직에서 불필요한 변수 삭제

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -6,14 +6,11 @@ let moneyArr = [];
 for (let i = 1; i <= n; i++) {
     moneyArr.push(Number(input[i]));
 }
-moneyArr.sort((a, b) => b - a);
 
-let result = 0;
-for (m of moneyArr) {
-    const count = parseInt(amount / m);
-    if (count > 0) {
-        result += count;
-        amount -= m * count;
-    }
+// 반복문의 변수를 활용하면 정렬하지 않아도 된다.
+let count = 0;
+for (let i = n - 1; i >= 0; i--) {
+    count += parseInt(amount / moneyArr[i]); // 해당 동전 개수
+    amount %= moneyArr[i]; // 해당 동전으로 나눈 나머지(잔액)
 }
-console.log(result);
+console.log(count);
