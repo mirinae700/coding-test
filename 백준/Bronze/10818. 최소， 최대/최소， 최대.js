e752ab885f1d4a0e4eba51700e18e852227baa29
Yujin Jeon <mirinae219@gmail.com>
2024-12-05 17:59:28 +0900

    [Bronze III] Title: 최소, 최대, Time: 376 ms, Memory: 94988 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const input = fs.readFileSync('dev/stdin').toString().split('\n');
+
+const arr = input[1].split(' ').map(Number);
+console.log(Math.min(...arr), Math.max(...arr));
