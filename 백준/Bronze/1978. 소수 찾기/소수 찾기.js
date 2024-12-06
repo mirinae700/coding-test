2d9838c18a5f198f21693c9977e632b683e81b15
Yujin Jeon <mirinae219@gmail.com>
2024-12-06 23:17:26 +0900

    [Bronze II] Title: 소수 찾기, Time: 148 ms, Memory: 9692 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.js
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.js
@@ -6,13 +6,16 @@ const arr = input[1].split(' ').map(Number);
 
 let result = 0;
 for (const num of arr) {
-    let flag = true;
-    for (let i = 2; i <= 1000; i++) {
-        if ((num !== i && num % i === 0) || num === 1) {
-            flag = false;
-            break;
-        }
-    }
-    if (flag) result += 1;
+    if (check(num)) result += 1;
 }
 console.log(result);
+
+function check(num) {
+    if (num < 2) return false;
+    else {
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) return false;
+        }
+    }
+    return true;
+}
