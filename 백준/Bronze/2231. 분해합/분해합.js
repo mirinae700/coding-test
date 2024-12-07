48e4b67de682c037c996569b0ededa7c0c3de575
Yujin Jeon <mirinae219@gmail.com>
2024-12-07 23:50:59 +0900

    [Bronze II] Title: 분해합, Time: 780 ms, Memory: 47320 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.js
+++ 백준/Bronze/2231. 분해합/분해합.js
@@ -2,15 +2,12 @@ const fs = require('fs');
 const input = fs.readFileSync('dev/stdin').toString().split('\n');
 
 const num = Number(input[0]);
-let flag = false;
 let result = 0;
 for (let i = 1; i < num; i++) {
     let cur = i + i.toString().split('').map(Number).reduce((a, b) => a + b);
     if (num === cur) {
         result = i;
-        flag = true;
         break;
     }
 }
-if (flag) console.log(result);
-else console.log(0);
+console.log(result);
