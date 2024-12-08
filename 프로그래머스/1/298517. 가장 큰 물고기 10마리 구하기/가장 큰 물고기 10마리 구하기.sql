4d886df5731b7a7e1167a84faad7767566aea615
Yujin Jeon <mirinae219@gmail.com>
2024-12-09 01:03:19 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, LIMIT 사용 추가

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,5 +1,13 @@
+-- 윈도우 함수 사용
 SELECT id, length
-FROM (SELECT ROW_NUMBER() OVER(ORDER BY length DESC) rn, id, length
-      FROM fish_info) t
-WHERE rn <= 10;
+FROM (SELECT ROW_NUMBER() OVER(ORDER BY length DESC, id ASC) RN,
+             id,
+             length
+      FROM fish_info) T
+WHERE RN <= 10;
 
+-- LIMIT 사용
+SELECT id, length
+FROM fish_info
+ORDER BY length DESC, id ASC
+LIMIT 10;
