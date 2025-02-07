b346cfc327927ea3a3176510cee03cb129aff027
Yujin Jeon <mirinae219@gmail.com>
2025-02-07 18:35:29 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 const [a, b] = input[0].split(" ").map(Number);
-
 console.log(a + b);
